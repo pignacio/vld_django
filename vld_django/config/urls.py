5c9ba40d84de6a58de094c9ef535bb65b4cbe81b
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-05-21 15:42:19 -0300

    Minor style fix

--- vld_django/config/urls.py
+++ vld_django/config/urls.py
@@ -28,10 +28,12 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
-                            url(r'^__debug__/', include(debug_toolbar.urls)),
-                            )
-    urlpatterns += patterns("",
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )  # yapf: disable
+    urlpatterns += patterns(
+        "",
         url(r"^media/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
         url(r"^static/(?P<path>.*)$", "django.views.static.serve", {"document_root": settings.STATIC_ROOT}),
     )  # yapf: disable
