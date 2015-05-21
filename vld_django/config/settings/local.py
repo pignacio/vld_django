45c207d245be52982526d5c0336aaf224231daf9
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-05-21 15:41:20 -0300

    Fix debug toolbar

--- vld_django/config/settings/local.py
+++ vld_django/config/settings/local.py
@@ -60,16 +60,16 @@ COMPRESS_OUTPUT_DIR = ''
 
 ########## TOOLBAR CONFIGURATION
 # See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
- #  INSTALLED_APPS += (
- #      'debug_toolbar',
- #  )
- #
- #  MIDDLEWARE_CLASSES += (
- #      'debug_toolbar.middleware.DebugToolbarMiddleware',
- #  )
- #
- #  DEBUG_TOOLBAR_PATCH_SETTINGS = False
- #
- #  # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
- #  INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+INSTALLED_APPS += ('debug_toolbar', )
+
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+DEBUG_TOOLBAR_CONFIG = {
+  'SHOW_TOOLBAR_CALLBACK': lambda *args, **kwargs: True,
+}
+
 ########## END TOOLBAR CONFIGURATION
