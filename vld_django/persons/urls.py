66d11e73760746a93f4128871ea9065074203dbe
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-05-28 17:08:43 -0300

    persons: values manager

--- vld_django/persons/urls.py
+++ vld_django/persons/urls.py
@@ -18,4 +18,8 @@ urlpatterns = patterns(  # pylint: disable=invalid-name
     url(r'^(?P<person_name>\w+)/import$', views.PersonImportView.as_view(), name='import'),
     url(r'^(?P<person_name>\w+)/update$', views.PersonUpdateView.as_view(), name='update'),
     url(r'^(?P<person_name>\w+)$', views.PersonDetailView.as_view(), name='detail'),
+    url(r'^(?P<person_name>\w+)/values$', views.PersonValuesView.as_view(), name='values'),
+    url(r'^(?P<person_name>\w+)/values/create$', views.PersonCreateValueView.as_view(), name='create_value'),
+    url(r'^(?P<person_name>\w+)/values/select_date$', views.PersonValuesSelectDatesView.as_view(), name='select_date'),
+    url(r'^(?P<person_name>\w+)/values/add/(?P<date_start>\d{4}-\d{2}-\d{2})/(?P<date_end>\d{4}-\d{2}-\d{2})$', views.PersonAddValuesView.as_view(), name='add_values'),
 )  # yapf: disable
--- vld_django/persons/views.py
+++ vld_django/persons/views.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals, division
 
+import datetime
 import logging
 
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
-from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, FormView
 
 from utils.views import LoginRequiredMixin
 from meals.models import Meal
 from meals.helper import trim_meals_data
 from .models import Person
-from .forms import PersonImportForm, PersonUpdateForm
+from .forms import PersonImportForm, PersonUpdateForm, PersonCreateValueForm, PersonValuesSelectDatesForm, PersonAddValuesForm
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
@@ -72,3 +73,53 @@ class PersonImportView(LoginRequiredMixin, UpdateView):
 
 class PersonListView(LoginRequiredMixin, ListView):
     model = Person
+
+
+class PersonValuesView(LoginRequiredMixin, DetailView):
+    model = Person
+    pk_url_kwarg = 'person_name'
+    template_name_suffix = '_values'
+
+
+class PersonCreateValueView(LoginRequiredMixin, UpdateView):
+    model = Person
+    pk_url_kwarg = 'person_name'
+    form_class = PersonCreateValueForm
+    template_name_suffix = '_create_value'
+
+    def form_valid(self, form):
+        self.object.values[form.cleaned_data['name']] = {}
+        self.object.save()
+        return redirect('persons:values', self.object.name)
+
+
+class PersonValuesSelectDatesView(LoginRequiredMixin, UpdateView):
+    model = Person
+    pk_url_kwarg = 'person_name'
+    form_class = PersonValuesSelectDatesForm
+    template_name_suffix = '_select_dates'
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        return redirect('persons:add_values', self.object.name, data['date_start'],
+                        data['date_end'])
+
+
+class PersonAddValuesView(LoginRequiredMixin, UpdateView):
+    model = Person
+    pk_url_kwarg = 'person_name'
+    form_class = PersonAddValuesForm
+    template_name_suffix = '_add_values'
+
+    def get_form_kwargs(self, *args, **kwargs):
+        res = super(PersonAddValuesView, self).get_form_kwargs(*args, **kwargs)
+        res['date_start'] = datetime.datetime.strptime(self.kwargs['date_start'], '%Y-%m-%d')
+        res['date_end'] = datetime.datetime.strptime(self.kwargs['date_end'], '%Y-%m-%d')
+        return res
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        for date, value, field_name in form.get_date_value_field_triplets():
+            self.object.values[value][date] = data[field_name]
+        self.object.save()
+        return redirect('persons:values', self.object.name)
