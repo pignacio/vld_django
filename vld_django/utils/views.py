b3a65104ec7bed4e2b4da24a32bd80dd7a877ccc
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-07-19 18:11:06 -0300

    utils: SimpleFormViewMixin

--- vld_django/utils/views.py
+++ vld_django/utils/views.py
@@ -14,3 +14,16 @@ class LoginRequiredMixin(object):
     def as_view(cls, **initkwargs):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
+
+
+class SimpleFormViewMixin(object):
+    template_name = 'simple_form.html'
+    page_title = 'Page title'
+
+    def get_page_title(self):
+        return self.page_title
+
+    def get_context_data(self, *args, **kwargs):
+        data = super(SimpleFormViewMixin, self).get_context_data(*args, **kwargs)
+        data['page_title'] = self.get_page_title()
+        return data
